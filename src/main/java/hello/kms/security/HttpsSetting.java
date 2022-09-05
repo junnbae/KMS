6c76a3e2df809d9a3aab9c34bbd60b05a04591c3
junnbae <uyiuy97@naver.com>
2022-09-05 20:13:38 +0900

    http to https redirect

--- src/main/java/hello/kms/security/HttpsSetting.java
+++ src/main/java/hello/kms/security/HttpsSetting.java
@@ -1,46 +1,46 @@
-//package hello.kms.security;
-//
-//import org.apache.catalina.Context;
-//import org.apache.catalina.connector.Connector;
-//import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-//import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-//import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-//import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-//import org.springframework.context.annotation.Bean;
-//
-//public class HttpsSetting {
-//    @Bean
-//    public ServletWebServerFactory servletWebServerFactory(){
-//        // Enable SSL Traffic
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//
-//        // Add HTTP to HTTPS redirect
-//        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
-//
-//        return tomcat;
-//    }
-//
-//    /*
-//    We need to redirect from HTTP to HTTPS. Without SSL, this application used
-//    port 8080. With SSL it will use port 443. So, any request for 8080 needs to be
-//    redirected to HTTPS on 443.
-//     */
-//    private Connector httpToHttpsRedirectConnector() {
-//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-//        connector.setScheme("http");
-//        connector.setPort(8080);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
-//}
+package hello.kms.security;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+public class HttpsSetting {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        // Enable SSL Traffic
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /*
+    We need to redirect from HTTP to HTTPS. Without SSL, this application used
+    port 8080. With SSL it will use port 443. So, any request for 8080 needs to be
+    redirected to HTTPS on 443.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+}

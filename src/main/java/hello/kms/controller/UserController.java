d59b7a3a16974c34feddeec5ca32c820495af962
junnbae <uyiuy97@naver.com>
2022-11-15 18:31:29 +0900

    complete kakao oauth & add naver oauth(1)

--- src/main/java/hello/kms/controller/UserController.java
+++ src/main/java/hello/kms/controller/UserController.java
@@ -17,28 +17,36 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/checkId")
-    @ResponseBody
-    public HashMap<String, Boolean> checkId(@RequestParam String id){
-        return userService.validateDuplicateId(id);
-    }
-
-    @PostMapping("/signUp")
-    @ResponseBody
-    public HashMap<String, Boolean> signUp(@RequestBody RegisterUserForm form){
-        return userService.register(form);
-    }
-
-    @PostMapping("/login")
-    @ResponseBody
-    public String signIn(@RequestBody LoginUserForm form, HttpServletResponse response){
-        return userService.login(form);
-    }
+//    @PostMapping("/checkId")
+//    @ResponseBody
+//    public HashMap<String, Boolean> checkId(@RequestParam String id){
+//        return userService.validateDuplicateId(id);
+//    }
+//
+//    @PostMapping("/signUp")
+//    @ResponseBody
+//    public HashMap<String, Boolean> signUp(@RequestBody RegisterUserForm form){
+//        return userService.register(form);
+//    }
+//
+//    @PostMapping("/login")
+//    @ResponseBody
+//    public String signIn(@RequestBody LoginUserForm form, HttpServletResponse response){
+//        return userService.login(form);
+//    }
 
     @GetMapping("/login/oauth2/code/kakao")
     @ResponseBody
     public String kakaoLogin(@RequestParam String code){
         return userService.kakaoLogin(code);
+//        https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Flocalhost%252Flogin%252Foauth2%252Fcode%252Fkakao%26through_account%3Dtrue%26client_id%3D43e398315e08268714887b3750a55107
+    }
+
+    @GetMapping("/login/oauth2/code/naver")
+    @ResponseBody
+    public String naverLogin(@RequestParam String code){
+        return userService.naverLogin(code);
+//        https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=I7UExJKBf_S3n8z2QDw3&redirect_uri=https://localhost/login/oauth2/code/naver&state=
     }
 
     @GetMapping("/admin/adminUser")
--- src/main/java/hello/kms/service/UserService.java
+++ src/main/java/hello/kms/service/UserService.java
@@ -1,9 +1,6 @@
 package hello.kms.service;
 
-import hello.kms.dto.LoginUserForm;
-import hello.kms.dto.RegisterUserForm;
 import hello.kms.domain.User;
-import hello.kms.exception.*;
 import hello.kms.repository.UserRepository;
 import hello.kms.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
 import static hello.kms.service.GetApiByUrl.getStringFromAPI;
@@ -33,87 +29,134 @@ public class UserService {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String clientId;
+    private String kakaoClientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-url}")
+    private String kakaoRedirectUrl;
+    @Value("${spring.security.oauth2.client.provider.kakao.token-url}")
+    private String kakaoTokenHostUrl;
+    @Value("${spring.security.oauth2.client.provider.kakao.user-info-url}")
+    private String kakaoUserInfoUrl;
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-grant-type}")
+    private String kakaoAuthorizationType;
 
-    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String redirectUrl;
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    private String naverClientId;
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-url}")
+    private String naverRedirectUrl;
+    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
+    private String naverClientSecret;
+    @Value("${spring.security.oauth2.client.provider.naver.token-url}")
+    private String naverTokenHostUrl;
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-grant-type}")
+    private String naverAuthorizationType;
 
-    public HashMap<String, Boolean> register(RegisterUserForm form) {
-        if(!validateDuplicateId(form.getUserId()).get("validate")){
-            throw new IDExistException();
-        }
-        form.setPassword(bCryptPasswordEncoder.encode(form.getPassword()));
+//    public HashMap<String, Boolean> register(RegisterUserForm form) {
+//        if(!validateDuplicateId(form.getUserId()).get("validate")){
+//            throw new IDExistException();
+//        }
+//        form.setPassword(bCryptPasswordEncoder.encode(form.getPassword()));
+//
+//        User user = User.builder()
+//                .userId(form.getUserId())
+//                .password(form.getPassword())
+//                .userName(form.getUserName())
+//                .roles(Collections.singletonList("ROLE_USER"))
+//                .build();
+//
+//        userRepository.save(user);
+//
+//        HashMap<String, Boolean> map = new HashMap<>();
+//        map.put("signUpSuccess", true);
+//        return map;
+//    }
+//
+//    public HashMap<String, Boolean> validateDuplicateId(String memberId){
+//        HashMap<String, Boolean> map = new HashMap<>();
+//
+//        if(userRepository.findByUserId(memberId).isPresent()){
+//            map.put("validate", false);
+//        }else {
+//            map.put("validate", true);
+//        }
+//        return map;
+//    }
+//
+//    public String login(LoginUserForm form){
+//        User user = userRepository.findByUserId(form.getUserId()).orElseThrow(UserNotExistException::new);
+//
+//        if(bCryptPasswordEncoder.matches(form.getPassword(), user.getPassword())){
+//            return jwtTokenProvider.createAccessToken(user.getUserId(), user.getRoles());
+//        }
+//
+//        throw new PasswordWrongException();
+//    }
 
-        User user = User.builder()
-                .userId(form.getUserId())
-                .password(form.getPassword())
-                .userName(form.getUserName())
-                .roles(Collections.singletonList("ROLE_USER"))
-                .build();
-
-        userRepository.save(user);
-
-        HashMap<String, Boolean> map = new HashMap<>();
-        map.put("signUpSuccess", true);
-        return map;
-    }
-
-    public HashMap<String, Boolean> validateDuplicateId(String memberId){
-        HashMap<String, Boolean> map = new HashMap<>();
-
-        if(userRepository.findByUserId(memberId).isPresent()){
-            map.put("validate", false);
-        }else {
-            map.put("validate", true);
-        }
-        return map;
-
-//        userRepository.findByUserId(memberId)
-//                .ifPresent(m -> {
-//                    throw new IDExistException();
-//                });
-    }
-
-    public String login(LoginUserForm form){
-        User user = userRepository.findByUserId(form.getUserId()).orElseThrow(UserNotExistException::new);
-
-        if(bCryptPasswordEncoder.matches(form.getPassword(), user.getPassword())){
-            return jwtTokenProvider.createAccessToken(user.getUserId(), user.getRoles());
-        }
-
-        throw new PasswordWrongException();
-    }
-
-    public String kakaoLogin(String code){
-        String tokenUrl="https://kauth.kakao.com/oauth/token?"
-                + "grant_type=authorization_code"
-                + "&client_id="+clientId
-                + "&redirect_url="+redirectUrl
-                + "&code="+code;
-
+    public String getAccessToken(String code, String tokenUrl){
         try {
             String body = getStringFromAPI(tokenUrl);
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(body);
-            String accessToken = String.valueOf(jsonObject.get("access_token"));
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Authorization", "Bearer " + accessToken);
-
-            HttpEntity<String> request = new HttpEntity<>(null, headers);
-            RestTemplate restTemplate = new RestTemplate();
-            String res = restTemplate.postForObject(
-                    "https://kapi.kakao.com/v2/user/me",
-                    request,
-                    String.class
-            );
-            jsonObject = (JSONObject) jsonParser.parse(res);
-            return String.valueOf(jsonObject);
-//            return body;
-
+            return String.valueOf(jsonObject.get("access_token"));
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public JSONObject kakaoUserId(String accessToken){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<String> request = new HttpEntity<>(null, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        String res = restTemplate.postForObject(
+                kakaoUserInfoUrl,
+                request,
+                String.class
+        );
+
+        JSONParser jsonParser = new JSONParser();
+        try {
+            return (JSONObject) jsonParser.parse(res);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String kakaoLogin(String code){
+        String tokenUrl = kakaoTokenHostUrl
+                + "?grant_type=" + kakaoAuthorizationType
+                + "&client_id=" + kakaoClientId
+                + "&redirect_url=" + kakaoRedirectUrl
+                + "&code=" + code;
+        String accessToken = getAccessToken(code, tokenUrl);
+
+        JSONObject userInfo = kakaoUserId(accessToken);
+        String userId = String.valueOf(userInfo.get("id"));
+
+        User user = userRepository.findByUserId(userId).orElseGet(() ->{
+            JSONObject tmp = (JSONObject) userInfo.get("properties");
+            String nickname = String.valueOf(tmp.get("nickname"));
+
+            return User.builder()
+                    .userId(userId)
+                    .password("")
+                    .userName(nickname)
+                    .roles(Collections.singletonList("ROLE_USER"))
+                    .build();
+        });
+
+        return jwtTokenProvider.createAccessToken(user.getUserId(), user.getRoles());
+    }
+
+    public String naverLogin(String code){
+        String tokenUrl = naverTokenHostUrl
+                + "?grant_type=" + naverAuthorizationType
+                + "&client_id=" + naverClientId
+                + "&redirect_url=" + naverRedirectUrl
+                + "&code=" + code;
+        String accessToken = getAccessToken(code, tokenUrl);
+        return code;
     }
 
     public List<User> adminUser(){
